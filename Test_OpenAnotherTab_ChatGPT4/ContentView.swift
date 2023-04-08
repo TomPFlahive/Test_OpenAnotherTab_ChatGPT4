c42642ccbc8822ba87fc473b61d6b88967eff0d0
Tom Flahive <TomPFlahive@gmail.com>
2023-04-08 08:47:51 -0700

    Initial Commit

---
+++ Test_OpenAnotherTab_ChatGPT4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test_OpenAnotherTab_ChatGPT4
+//
+//  Created by Tom Flahive on 4/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test_OpenAnotherTab_ChatGPT4/Test_OpenAnotherTab_ChatGPT4App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_OpenAnotherTab_ChatGPT4App.swift
+//  Test_OpenAnotherTab_ChatGPT4
+//
+//  Created by Tom Flahive on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_OpenAnotherTab_ChatGPT4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
